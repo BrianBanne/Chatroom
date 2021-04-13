2bc3de065ce9e76d40629930e5ce7d5e4ceeb826
BrianBanne <60739318+BrianBanne@users.noreply.github.com>
2021-04-13 16:59:26 +0200

    sett litt på bots, ser det en grei metode eller? var litt usikker på hvordan man skulle ta å se over meldingene som kommer over "streamen", så blir kanskje litt feil med getRoomMessages siden den returnerer alle meldinger. og da kan det skje funky ting hvis botten joiner sent i samtalen

--- client/scripts/bot.js
+++ client/scripts/bot.js
@@ -1,19 +1,44 @@
+import {getRoomMessages, sendMessage} from "../api";
+
 class Bot {
+
     constructor(name){
         this.name = name
         this.favourite = {}
     }
 
-    load(path){
+    async load(self, path) {
         const file = await fetch(path).then(res => res.json())
+        let data = JSON.load(file);
         console.log(file);
+        name = self.name.lower(); //???
+
+        let botdata = {};
+
+
+        if (name === "Wisdombot" || name === "Foodbot" || name === "chairdude") {
+            botdata = data[name];
+        } else {
+            botdata=data["basicbot"];
+        }
+
 
         //log file. to this.
     }
 
-    respond(input) {
-        // do something
+    respond(self, input) {
+        if (readmessages().includes("hello") || readmessages().includes("hi")) {
+            sendMessage(roomId, userId, self.greetings.values())
+        }
+        //if read includes bye "botname -- leave
+        // grei mal å gå ut ifra eller hva tenker du? hentet litt inspo fra ditt forrige prosjekt
+        //og merga det med mine metoder :P
     }
 
+
+
+}
+function readmessages() {
+   return getRoomMessages();
 
 }
