1f403a0697612732420cbf5c37cd1f9fdd3b22d7
BrianBanne <60739318+BrianBanne@users.noreply.github.com>
2021-04-14 15:42:18 +0200

    shitty shit, but whatever

--- client/scripts/bot.js
+++ client/scripts/bot.js
@@ -5,9 +5,49 @@ class Bot {
     this.name = name;
     this.id = "";
     this.roomId = roomId;
-    //if name == blabla this.options = custom shit for hver bot
-    //custom for each json thingy
-    this.greetings = data.Foodbot.greetings;
+    if (name === "Wisdombot") {
+      this.catchphrase = data.Wisdombot.catchphrase.values;
+      this.mood = data.Wisdombot.mood;
+      this.topics = data.Wisdombot.topics;
+      this.explanation = data.Wisdombot.explanation;
+      this.greetings = data.Wisdombot.greetings;
+      this.composer = data.Wisdombot.composer;
+      this.wildcards = data.Wisdombot.wildcards;
+      this.badword = data.Wisdombot.meanings.bad;
+      this.goodword = data.Wisdombot.meanings.good;
+    } else if (name === "Foodbot") {
+      this.food = data.Foodbot.food;
+      this.mood = data.Foodbot.mood;
+      this.topics = data.Foodbot.topics;
+      this.explanation = data.Foodbot.explanation;
+      this.greetings = data.Foodbot.greetings;
+      this.wildcards = data.Foodbot.wildcards;
+      this.badword = data.Foodbot.meanings.bad;
+      this.goodword = data.Foodbot.meanings.good;
+    } else if(name === "Chairbot") {
+      this.catchphrase = data.Chairbot.chairs.values;
+      this.mood = data.Chairbot.mood;
+      this.greetings = data.Chairbot.greetings;
+      this.wildcards = data.Chairbot.wildcards;
+      this.badword = data.Chairbot.meanings.bad;
+      this.goodword = data.Chairbot.meanings.good;
+    } else if (name === "basicbot") {
+      this.SWchara = data.basicbot.starwars.characters;
+      this.catchphrase = data.basicbot.starwars.force;
+      this.SWmovies = data.basicbot.starwars.movies;
+      this.topics = data.basicbot.hobbies.interests;
+      this.explanation = data.basicbot.hobbies.explanation;
+      this.billgatesfacts = data.basicbot.billgates;
+      this.drinkwater = data.basicbot.water.drinkmore;
+      this.albums = data.basicbot.favorite.discography;
+      this.movies = data.basicbot.favorite.movies;
+      this.computerparts = data.basicbot.favorite.computerparts;
+      this.wildcards = data.basicbot.wildcards;
+      this.badmood = data.basicbot.mood.bad;
+      this.goodmood = data.basicbot.mood.good;
+      this.explanation = data.basicbot.explain;
+
+    }
     this.options = ["Hello", "How are you", "Im fine", "Fockk off", 'Wazzao', 'Im tired', 'Jadda', 'Hurray', 'I love ndoe', 'I love Java'];
   }
 
